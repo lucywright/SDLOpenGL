fcb70c26b8988367cb03790521766f085ec15be3
lucywright <i7671811@bournemouth.ac.uk>
2016-02-22 11:40:36 +0000

    commit with window opening

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,32 @@
 #include <iostream>
+#include "SDLOpenGL.h"
+//adding NGL things
+#include <ngl/NGLInit.h>
+
+void initializeGL();
+void renderGL();
 
 int main(int argc, char *argv[])
 {
-  std::cout<<"working"<<std::endl;
+  SDLOpenGL window ("test",0,0,1024,720);
+  window.makeCurrent();
+  initializeGL();
+  while(true)
+  {
+    renderGL();
+    window.swapWindow();
+    SDL_Delay(10);
+  }
+  //std::cout<<"working"<<std::endl;
+}
+
+void initializeGL()
+{
+  ngl::NGLInit::instance();
+  glClearColor(0.8,0.8,0.8,1.0);
+}
+
+void renderGL()
+{
+  glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 }
